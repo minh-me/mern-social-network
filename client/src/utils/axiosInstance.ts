4e9b1d69b2893da6404a376df7989d2ef706b49f
minhchiu <minh.mchiu@gmail.com>
2022-05-27 09:20:37 +0700

    Fix refresh token in client

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -13,19 +13,18 @@ const config: AxiosRequestConfig = {
 const axiosInstance: AxiosInstance = axios.create(config);
 
 axiosInstance.interceptors.request.use(async (req: AxiosRequestConfig<AxiosRequestHeaders>) => {
-  if (!ac_token) {
-    ac_token = storage.getToken() || '';
-  }
+  if (!ac_token) ac_token = storage.getToken() || '';
+
   req.headers = { ...req.headers };
   req.headers.Authorization = `Bearer ${ac_token}`;
+
   const user: { exp: number; sub: string; iat: string } = jwt_decode(ac_token);
 
   const isExpired = dayjs.unix(user.exp).diff(dayjs()) < 1;
+
   if (!isExpired) return req;
 
-  const data: AuthResponse = await axios.get(`/api/auth/rf_token`, {
-    withCredentials: true,
-  });
+  const data: AuthResponse = await axios.get(`/api/auth/rf_token`, { withCredentials: true });
 
   storage.setToken(data.ac_token);
   req.headers.Authorization = `Bearer ${data.ac_token}`;
